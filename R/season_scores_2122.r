aff83d84f6ebaf0aac711bf5c771ba5d65bf8426
australis <7326380+openfields@users.noreply.github.com>
2023-07-19 09:19:47 -0500

    compile scores for season

---
+++ R/season_scores_2122.r
@@ -0,0 +1,11 @@
+# game score compilation
+
+oc.g <- game.scores("nhl_data/oct21.rds") 
+no.g <- game.scores("nhl_data/nov21.rds")
+de.g <- game.scores("nhl_data/dec21.rds")
+ja.g <- game.scores("nhl_data/jan22.rds") # row 190, these are 0-0 games at the end of regulation
+fe.g <- game.scores("nhl_data/feb21.rds")
+ma.g <- game.scores("nhl_data/mar22.rds") # row 132
+ap.g <- game.scores("nhl_data/apr22.rds") # row 87
+apx.g <- game.scores("nhl_data/apr112022.rds") 
+may.g <- game.scores("nhl_data/may012022.rds")
